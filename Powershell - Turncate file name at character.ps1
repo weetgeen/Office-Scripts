8e188b5d0a7f756f38db16057003f548cde30120
Hans Pot <hans-pot@hotmail.com>
2018-10-27 11:19:38 +0200

    Update Powershell - Turncate file name at character.ps1

--- Powershell - Turncate file name at character.ps1
+++ Powershell - Turncate file name at character.ps1
@@ -2,14 +2,14 @@
 $character = ' '                            #Charachter on which to turncate filename
 $destinationFolder = "C\Destination"
 
-ForEach($foto in $sourceFolder)
+ForEach($file in $sourceFolder)
 {
-    $extension = $foto.Extension
+    $extension = $file.Extension
 
-    $name =  $foto.Basename.Substring(0,$foto.basename.IndexOf($character))  #Turncate name by $character
+    $name =  $file.Basename.Substring(0,$file.basename.IndexOf($character))  #Turncate name by $character
 
 
-    $foto | Copy-Item -destination "$Destination\$name$extension"  #Copy image with new Filename
+    $file | Copy-Item -destination "$Destination\$name$extension"  #Copy image with new Filename
    
    
     
