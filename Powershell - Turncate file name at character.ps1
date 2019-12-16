a8c1d8e8303d89482e4e9e15ad8ef699643181d1
weetgeen <hans-pot@hotmail.com>
2019-12-16 09:42:54 +0100

    Blank spaces

--- Powershell - Turncate file name at character.ps1
+++ Powershell - Turncate file name at character.ps1
@@ -5,13 +5,6 @@ $destinationFolder = "C\Destination"
 ForEach($file in $sourceFolder)
 {
     $extension = $file.Extension
-
     $name =  $file.Basename.Substring(0,$file.basename.IndexOf($character))  #Turncate name by $character
-
-
     $file | Copy-Item -destination "$Destination\$name$extension"  #Copy image with new Filename
-   
-   
-    
-
 }
